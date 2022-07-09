eec948db326fdd6b601631a428a17f6290c93ac8
Sirat Baweja <sirat.b99@gmail.com>
2022-07-09 20:49:10 +0530

    security - pass and gpg keys

---
+++ gpg-keys.sh
@@ -0,0 +1,12 @@
+# create a key
+gpg --full-generate-key
+
+# print out key in long format
+gpg --list-secret-key --keyid-format LONG
+
+# remove a key
+gpg --delete-secret-and-public-key $KEY_NAME
+
+# decrypt key
+gpg -d $KEY_FILE
+# gpg -d <key>.gpg
---
+++ using-pass-as-a-passwd-manager.sh
@@ -0,0 +1,17 @@
+# initialize the passwd
+pass init $GPG_KEY
+
+# check the passwd store
+pass
+pass ls
+
+# add your passwd to the store
+pass insert $KEYNAME
+# can be the name of the website or service
+
+# view password store
+cd ~/.password-store
+vim $KEYNAME.gpg
+
+# view key
+pass $KEYNAME.gpg
